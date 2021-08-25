f880ac76508b74dad08969ff8cc77e1380f3ec40
Paweł Buderaski <pbuderaski@gmail.com>
2021-08-25 21:08:34 +0100

    feat: orientationChange listener (#457)

--- plugins/quick-brick-native-apple/apple/universal/ReactNative/ReactNativeModulesExports.m
+++ plugins/quick-brick-native-apple/apple/universal/ReactNative/ReactNativeModulesExports.m
@@ -109,3 +109,7 @@ RCT_EXTERN_METHOD(presentLocalNotification:(NSDictionary<NSString *, id> *) payl
                       resolver:(RCTPromiseResolveBlock)resolve
                           rejecter:(RCTPromiseRejectBlock)reject);
 @end
+
+@interface RCT_EXTERN_MODULE (ReactNativeEventEmitter, RCTEventEmitter)
+RCT_EXTERN_METHOD(supportedEvents)
+@end
